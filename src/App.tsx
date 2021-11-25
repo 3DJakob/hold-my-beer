f846ef8f587a918186782cb010eadcb3ab19b652
Jakob Unnebäck <jakob@folkdatorn.se>
2021-11-25 18:15:17 +0100

    Proof of concept

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import * as d3 from 'd3'
+import { useEffect, useState } from 'react'
+// @ts-expect-error
+import data from './utils/beers.csv'
+import { BeerStruct } from './utils/typings'
+// @ts-expect-error
+import { Corpus } from 'tiny-tfidf'
+import stopwords from 'stopwords-sv'
+import SquareLoader from 'react-spinners/SquareLoader'
+const allBeers: BeerStruct[] = []
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const loadBeers = async (): Promise<string[]> => {
+  const beers: string[] = []
+  // @ts-expect-error
+  await d3.csv(data, function (data: BeerStruct) {
+    allBeers.push(data)
+    const properties = [
+      data.productNameBold,
+      data.productNameThin,
+      data.categoryLevel1,
+      data.categoryLevel2,
+      data.categoryLevel3,
+      data.categoryLevel4,
+      data.usage,
+      data.taste
+    ]
+    beers.push(properties.join(' '))
+  })
+  return beers
 }
 
-export default App;
+const App: React.FC = () => {
+  const [corpus, setCorpus] = useState<any>()
+  const [loading, setLoading] = useState(true)
+  const [results, setResults] = useState<BeerStruct[]>([])
+
+  const createCorpus = async (): Promise<void> => {
+    const beers = await loadBeers()
+    const documentTitles = beers.map((beer, i) => i.toString())
+
+    const corpus = new Corpus(
+      documentTitles,
+      beers,
+      false,
+      stopwords
+    )
+    setCorpus(corpus)
+    corpus.getResultsForQuery('ale')
+    setLoading(false)
+  }
+
+  useEffect(() => {
+    createCorpus().catch(() => console.log('error loading corpus!'))
+  }, [])
+
+  const handleInput = (e: any): void => {
+    console.log('input started')
+    if (corpus != null) {
+      console.log('corpus not null')
+      const result: Array<Array<string|number>> = corpus.getResultsForQuery(e.target.value).slice(0, 10)
+      // console.log('done similarity')
+      // console.log(result.map(item => corpus.getDocument(item[0])))
+      // console.log(result.map(item => allBeers[Number(item[0])]))
+      setResults(result.map(item => allBeers[Number(item[0])]))
+    }
+  }
+
+  return (
+    <div className='App'>
+      <h1>Beer recommender </h1>
+      {loading && (
+        <>
+          <SquareLoader color='black' loading size={150} />
+          <p>Constructing corpus</p>
+        </>
+      )}
+      <input onChange={handleInput} placeholder='Search for a beer!' />
+      {results.map(beer => {
+        return (
+          <p key={beer.productId}>
+            {beer.productNameThin} {beer.productNameBold}
+          </p>
+        )
+      })}
+    </div>
+  )
+}
+
+export default App
---
+++ src/utils/typings.ts
@@ -0,0 +1,74 @@
+export interface ImageStruct {
+  imageUrl: string
+  fileType?: any
+  size?: any
+}
+
+export interface TasteClockStruct {
+  key: string
+  value: number
+}
+
+export interface BeerStruct {
+  productId: string
+  productNumber: string
+  productNameBold: string
+  productNameThin: string
+  category?: any
+  productNumberShort: string
+  producerName: string
+  supplierName: string
+  isKosher: boolean
+  bottleTextShort: string
+  restrictedParcelQuantity: number
+  isOrganic: boolean
+  isEthical: boolean
+  ethicalLabel?: any
+  isWebLaunch: boolean
+  productLaunchDate: Date
+  isCompletelyOutOfStock: boolean
+  isTemporaryOutOfStock: boolean
+  alcoholPercentage: number
+  volumeText: string
+  volume: number
+  price: number
+  country: string
+  originLevel1?: any
+  originLevel2?: any
+  categoryLevel1: string
+  categoryLevel2: string
+  categoryLevel3: string
+  categoryLevel4?: any
+  customCategoryTitle: string
+  assortmentText: string
+  usage: string
+  taste: string
+  tasteSymbols: string[]
+  tasteClockGroupBitter?: any
+  tasteClockGroupSmokiness?: any
+  tasteClockBitter: number
+  tasteClockFruitacid: number
+  tasteClockBody: number
+  tasteClockRoughness: number
+  tasteClockSweetness: number
+  tasteClockSmokiness: number
+  tasteClockCasque: number
+  assortment: string
+  recycleFee: number
+  isManufacturingCountry: boolean
+  isRegionalRestricted: boolean
+  packaging: string
+  isNews: boolean
+  images: ImageStruct[]
+  isDiscontinued: boolean
+  isSupplierTemporaryNotAvailable: boolean
+  sugarContent: number
+  sugarContentGramPer100ml: number
+  seal: any[]
+  vintage?: any
+  grapes: any[]
+  otherSelections?: any
+  tasteClocks: TasteClockStruct[]
+  color: string
+  dishPoints?: any
+}
