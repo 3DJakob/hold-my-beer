e6396e8662b8b34ca27d800fcc7f4795b95c4ed0
Jakob Unnebäck <jakob@folkdatorn.se>
2021-11-30 20:50:57 +0100

    Gh-pages build

--- src/App.tsx
+++ src/App.tsx
@@ -137,6 +137,15 @@ const App: React.FC = () => {
 
   return (
     <Container>
+      <link rel='apple-touch-icon' sizes='180x180' href='/icons/apple-touch-icon.png' />
+      <link rel='icon' type='image/png' sizes='32x32' href='/icons/favicon-32x32.png' />
+      <link rel='icon' type='image/png' sizes='16x16' href='/icons/favicon-16x16.png' />
+      <link rel='manifest' href='/icons/site.webmanifest' />
+      <link rel='mask-icon' href='/icons/safari-pinned-tab.svg' color='#ffb84b' />
+      <link rel='shortcut icon' href='/icons/favicon.ico' />
+      <meta name='msapplication-TileColor' content='#ffb84b' />
+      <meta name='msapplication-config' content='/icons/browserconfig.xml' />
+      <meta name='theme-color' content='#ff0000' />
       <link rel='preconnect' href='https://fonts.googleapis.com' />
       <link href='https://fonts.googleapis.com/css2?family=Nunito:wght@200;400;700&display=swap' rel='stylesheet' />
       <Title>Beer finder</Title>
